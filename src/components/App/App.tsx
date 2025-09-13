54d9d5fae4e6d7fcca6b68e53dff2b0eb45200ce
Pooya Tolideh <pooya.tolideh@gmail.com>
2025-09-12 20:15:01 -0400

    feat: generate first plain grid

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,9 +8,23 @@ type Piece = {
 type Square = Piece | null;
 
 export const App: React.FC = () => {
+  const [game, setGame] = useState<Square[][]>(() => {
+    return Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => null));
+  });
   return (
     <div>
-      <h1>App</h1>
+      <h1>Checkers Game</h1>
+      <div className="flex flex-col border border-black w-fit">
+        {game.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex border-t-black border-b-black">
+            {row.map((square, columnIndex) => (
+              <div key={columnIndex} className="border border-l-black border-r-black size-8">
+                {JSON.stringify(square)}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
