f57b14ded4538f2a54b94a21d6969131130ab79f
Pooya Tolideh <pooya.tolideh@gmail.com>
2025-09-13 11:59:32 -0400

    feat: add checkers board coloring logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,65 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
+
+type Color = 'red' | 'black';
 
 type Piece = {
-  color: 'red' | 'black';
+  color: Color;
   isKing: boolean;
 };
 
-type Square = Piece | null;
+type Square = {
+  x: number;
+  y: number;
+  color: Color;
+  piece: Piece | null;
+};
+
+interface State {
+  game: Square[][];
+}
+
+const getInitialGameState = () => {
+  const game: Square[][] = Array.from({ length: 8 }, () =>
+    Array.from({ length: 8 }, () => ({ x: 0, y: 0, color: 'red', piece: null })),
+  );
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      const square = game[x][y];
+      if ((x + y) % 2 !== 0) {
+        square.color = 'black';
+      }
+    }
+  }
+  return game;
+};
+
+const initialState = {
+  game: getInitialGameState(),
+};
+
+function reducer(state: State, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
 
 export const App: React.FC = () => {
-  const [game, setGame] = useState<Square[][]>(() => {
-    return Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => null));
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
       <h1>Checkers Game</h1>
       <div className="flex flex-col border border-black w-fit">
-        {game.map((row, rowIndex) => (
+        {state.game.map((row, rowIndex) => (
           <div key={rowIndex} className="flex border-t-black border-b-black">
             {row.map((square, columnIndex) => (
-              <div key={columnIndex} className="border border-l-black border-r-black size-8">
-                {JSON.stringify(square)}
+              <div
+                key={columnIndex}
+                className="border border-l-black border-r-black size-8"
+                style={{ backgroundColor: square.color }}
+              >
+                {''}
               </div>
             ))}
           </div>
